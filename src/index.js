309da5903de37d00c61b1beab1bdae77eae3ae83
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2025-06-13 12:15:31 -0500

    Añadiendo estilos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,51 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./index.css"
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
 function App(){
    return <div>
@@ -11,28 +57,50 @@ function App(){
 }
 
 function Menu (){
-    return <div>
+    return <main className="menu">
         <h2>Nuestro Menú</h2>
-        <Pizza/>
-    </div>
+        {pizzaData.map((Piza)=>{
+            return(
+            <Pizza key={Piza.name} pizza={Piza}/>
+            )
+        })}
+    </main>
 }
 
 function Header (){
-return <h1>Comida Rápida React Co.</h1>
+return( 
+<header className="header">
+    <h1>Comida Rápida React Co.</h1>
+</header>
+)
 }
 
 function Footer (){
-return <footer>{new Date().toLocaleTimeString()} Estamos atendiendo en este momento</footer>
+    const hour = new Date().getHours();
+    const openHour = 8;
+    const closedHour= 22;
+    const isOpen= hour >= openHour && hour<=closedHour;
+    console.log(isOpen);
+return( 
+<footer className="footer">
+    {new Date().toLocaleTimeString()} Estamos atendiendo en este momento
+</footer>
+)
 //Así creamos un componente sin jsx
     // return React.createElement('footer', null, "Estamos atendiendo en este momento")
 }
 
-function Pizza(){
+function Pizza({pizza}){
+    const {name, ingredients, price,photoName, soldOut}= pizza;
     return(
-        <div>
-            <img src="pizzas/focaccia.jpg" alt="Imagen de Pizza"/>
-         <h1>Foccacia</h1>
-         <p>Pan con aceite de oliva italiano</p>
+        <div className="pizza">
+            <img src={photoName} alt="Imagen de Pizza"/>
+            <div>
+                 <h3>{name}</h3>
+                 <p>{ingredients}</p>
+                 <span>{price + 3}</span>
+            </div>
+        
         </div>
     )
 }
